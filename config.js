f529adee3643e28e1bc8301d70308fa26091abed
evileangel <150454461+evileangel@users.noreply.github.com>
2024-03-14 22:59:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hello XÍU',
+    introDesc: `14/3 - HAPPY BIRTHDAY 💃`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'SANH NHỰT THỨ 21 - GIÀ RỒI NÈ',
+    desc: 'Chúc m sinh nhựt vui vẻ nha, mọi việc hanh thông, mọi chuyện suôn sẻ 🤗 ',
+    btnYes: 'Dễ thương quá',
+    btnNo: 'Sến z cha',
+    question:'T mới học nè thấy hay chưa',
+    btnReply: 'Nhập thử j đuy',
+    reply: 'Mốt đi cast model dùm t, thiệc 💪',
+    mess: 'Tuyệt cú mèo! Qùa sinh nhật công nghệ nè',
+    messDesc: 'BÀI HÁT MỪNG SINH NHỰT NÈ!',
+    btnAccept: 'Nhấp vô!!!',
+    messLink: 'https://www.youtube.com/watch?v=bJi8SWxFJcg' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
